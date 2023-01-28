882b0502930de4abf1e97851c6d04aa9d9207ed5
Anton <nokker@gmail.com>
2023-01-28 17:23:26 +0900

    add poh, gh ang ig links

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import utilStyles from "../styles/utils.module.css";
+import homeStyles from "../styles/Home.module.css";
 import Layout, { siteTitle } from "../components/layout";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import { FaInstagram, FaGithub } from "react-icons/fa";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -21,17 +23,50 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
 
+      <section>
+        <ul className={`${utilStyles.list} ${utilStyles.horizontalList}`}>
+          <li>
+            <Link href="#">
+              <FaInstagram /> Instagram{" "}
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <FaGithub /> Github{" "}
+            </Link>
+          </li>
+        </ul>
+      </section>
       <section className={utilStyles.headingMd}>
         <p>
           Hi, I'm Anton. I'm a fullstack developer and illustrator. Sometimes a
-          En/Ru/Jp translator as well. It's me who translated{" "}
-          <a href="https://www.systemax.jp/en/sai/">Easy Paint Tool SAI</a> into
-          Russian.
+          En/Ru/Jp translator as well. It's me who translated
+          <a href="https://www.systemax.jp/en/sai/">
+            {" "}
+            Easy Paint Tool SAI
+          </a>{" "}
+          into Russian.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      </section>
+
+      <section className={utilStyles.headingMd}>
+        <h2 className={utilStyles.headingLg}>My projects</h2>
+        <ul>
+          <div className={homeStyles.card}>
+            <Link href="#">Power of habits</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              A web app that helps to keep you following your goals in life
+            </small>
+          </div>
+          <div className={homeStyles.card}>
+            <Link href="#">My art store</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              Stickers, Posters, etc., made by me, in a web shop, made by me
+            </small>
+          </div>
+        </ul>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
---
+++ posts/p-o-h-about.md
@@ -0,0 +1,15 @@
+---
+title: 'About my "Power of habits" project'
+date: "2023-01-28"
+---
+
+I made this project to deal with all the New Year's resolutions, thal lasts only for a couple of weeks, at the longest.
+
+- Want to get a sixpack? - Do some excersise for it for at least [22 days in a row](https://www.youtube.com/watch?v=F6PhNnlb-14)
+- Want get better at drawing? - Draw every day for at least a month! (Inktober)
+- Want that private island? Err... well, you've got my point.
+
+It's a calendar-like web app, where you can easily set your goal's continuity, and see how you are progressing!
+
+- Easy login with Google
+- Completely free!
